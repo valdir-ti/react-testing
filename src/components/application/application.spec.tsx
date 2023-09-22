acfca727e06dea7db0b33f68403c7c0f6560f324
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-09-22 10:49:46 -0300

    test: find with get by label text

--- src/components/application/application.spec.tsx
+++ src/components/application/application.spec.tsx
@@ -20,6 +20,11 @@ describe("<Application />", () => {
     });
     expect(nameElement).toBeInTheDocument();
 
+    const nameElement2 = screen.getByLabelText("Name", {
+      slector: "input",
+    });
+    expect(nameElement2).toBeInTheDocument();
+
     const bioElement = screen.getByRole("textbox", {
       name: "Bio",
     });
@@ -30,6 +35,11 @@ describe("<Application />", () => {
 
     const termsElement = screen.getByRole("checkbox");
     expect(termsElement).toBeInTheDocument();
+
+    const termsElement2 = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    );
+    expect(termsElement2).toBeInTheDocument();
 
     const submitButtonElement = screen.getByRole("button");
     expect(submitButtonElement).toBeInTheDocument();
