6f7ddf980cfc8ebecb2b84cc4ea5d46c89b875fa
Valdir Silva <valdir.ti@gmail.com>
2023-09-23 21:37:12 -0300

    test: keyboard interactions

--- src/components/counter/counter.spec.tsx
+++ src/components/counter/counter.spec.tsx
@@ -41,4 +41,34 @@ describe("<Counter />", () => {
     const countElement = screen.getByRole("heading");
     expect(countElement).toHaveTextContent("2");
   });
+
+  it("should renders a count of 10 after click the set button", async () => {
+    userEvent.setup();
+    render(<Counter />);
+    const amountInput = screen.getByRole("spinbutton");
+    await userEvent.type(amountInput, "10");
+    expect(amountInput).toHaveValue(10);
+    const setButton = screen.getByRole("button", {
+      name: "Set",
+    });
+    await userEvent.click(setButton);
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("10");
+  });
+
+  it("should elements are focused in the right order", async () => {
+    userEvent.setup();
+    render(<Counter />);
+    const amountInput = screen.getByRole("spinbutton");
+    const setButton = screen.getByRole("button", {
+      name: "Set",
+    });
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+    await userEvent.tab();
+    expect(incrementButton).toHaveFocus();
+    await userEvent.tab();
+    expect(amountInput).toHaveFocus();
+    await userEvent.tab();
+    expect(setButton).toHaveFocus();
+  });
 });
