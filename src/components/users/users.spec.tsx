b7865a132120653c27b3f053e84c5e9b218dab48
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-09-25 14:55:07 -0300

    test: msw error handle

--- src/components/users/users.spec.tsx
+++ src/components/users/users.spec.tsx
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react"
+import { server } from "../../mocks/server"
+import { rest } from 'msw'
 import { Users } from "./users"
 
 describe('<Users />', () => {
@@ -12,5 +14,18 @@ describe('<Users />', () => {
         render(<Users />)
         const users = await screen.findAllByRole('listitem')
         expect(users).toHaveLength(3)
+    })
+
+    it('should renders the error message', async () => {
+        server.use(
+            rest.get('https://jsonplaceholder.typicode.com/users',
+                (req, res, ctx) => {
+                    return res(ctx.status(500))
+                }
+            )
+        )
+        render(<Users />)
+        const error = await screen.findByText('Error fetching users')
+        expect(error).toBeInTheDocument()
     })
 })
